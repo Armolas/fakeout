21f9b3a9ed5ebb96625d60c7446d7af18e49359b
Armolas <armolas06@gmail.com>
2026-05-21 11:46:59 +0100

    feat: update Celo Sepolia chainId to use dynamic value from wagmi

--- fakeout-frontend/src/config/web3auth.ts
+++ fakeout-frontend/src/config/web3auth.ts
@@ -3,11 +3,11 @@ import { AuthAdapter } from '@web3auth/auth-adapter'
 import { EthereumPrivateKeyProvider } from '@web3auth/ethereum-provider'
 import { CHAIN_NAMESPACES, WEB3AUTH_NETWORK } from '@web3auth/base'
 import { Web3AuthConnector } from '@web3auth/web3auth-wagmi-connector'
+import { celoSepolia } from 'wagmi/chains'
 
-// Celo Sepolia = 44787 = 0xAEF3
 const chainConfig = {
   chainNamespace: CHAIN_NAMESPACES.EIP155,
-  chainId: '0xAEF3',
+  chainId: '0x' + celoSepolia.id.toString(16),
   rpcTarget: 'https://forno.celo-sepolia.celo-testnet.org',
   displayName: 'Celo Sepolia',
   ticker: 'CELO',
